553c1f2ea613266ed14d35f681895b985cd24022
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-16 22:33:12 +0000

    Using puts to write programs

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Uses write to print output to the stdout
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
