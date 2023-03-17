040dfe5ee5ef981cf3ef5c9e8fe2ca066058aead
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-17 18:09:33 +0000

    Positive and negative values

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Print random numbers
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d\n", num)
+
+	}
+	return (0);
+}
