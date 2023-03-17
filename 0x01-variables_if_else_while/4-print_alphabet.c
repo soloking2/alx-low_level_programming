d6cf3dc75d2d4a78d0a0e1ae6e1b4da2e4f387f6
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-17 17:48:21 +0000

    Positive and negative values

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Print random numbers
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'q' || alpha != 'e')
+		{
+			putchar(alpha);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
