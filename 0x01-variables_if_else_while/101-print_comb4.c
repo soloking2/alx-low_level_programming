6178c0c145474ff47d6b1f0c71c116e9a696ace6
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-17 19:55:47 +0000

    Positive and negative values

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,7 +14,7 @@ int main(void)
 	{
 		for (j = i + 1; j < 9; j++)
 		{
-			for(k = j + 1; k < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
