fc5b21ff84019f3ed9c6c7bbca4008d044504902
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-17 19:40:38 +0000

    Positive and negative values

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,19 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = i + 1; j <= 100; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			putchar('0' + i + j);
-			if (i > j || i == j)
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 8 && j == 9)
 			{
 				break;
 			}
+			putchar(',');
+			putchar(' ');
 		}
-		putchar(',');
-		putchar(' ');
 
 	}
 	putchar('\n');
