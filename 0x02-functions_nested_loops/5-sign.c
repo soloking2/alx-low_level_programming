ed37a458a7dad27d8f932b476267f29086fa534a
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-21 23:11:37 +0000

    Printing _putchar

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_sign - Entry point
+ *@n: single number input
+ *The function prints to the output
+ *Return: 1 if n is greater 1, else if 0 , else -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	} else if (n < -1)
+	{
+		_putchar('-1');
+		return (-1);
+	}
+}
