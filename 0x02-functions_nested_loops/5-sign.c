12eb97930d37cc68aadcd80e4e360342867ee8eb
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-21 23:44:32 +0000

    Printing _putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,9 +16,7 @@ int print_sign(int n)
 	{
 		_putchar('0');
 		return (0);
-	} else
-	{
-		_putchar('-');
-		return (-1);
 	}
+	_putchar('-');
+	return (-1);
 }
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_last_digit - Entry point
+ *@n: single number input
+ *The function prints to the output
+ *Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+
+	return (n);
+}
