eef897fe45a54c83d6c76a421c98a4793950f990
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-22 01:19:38 +0000

    Printing _putchar

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_times_table - Entry point
+ *@n: input
+ *The function prints to the output
+ */
+void print_times_table(int n)
+{
+	int a, i, result;
+
+	for (a = 0; a <= n; a++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (i = 1; i <= n; i++)
+		{
+			result = i * a;
+
+			if ((result / 10) > 0)
+			{
+				_putchar((result / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((result % 10) + '0');
+			if (i < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
