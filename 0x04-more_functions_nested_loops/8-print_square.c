e6abaefe72c33b2afdd8ac938d943449c7cbf23f
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-23 21:39:35 +0000

    Uppercase addition

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,7 +7,7 @@
  */
 void print_square(int size)
 {
-	int i;
+	int i, y;
 
 	if (size <= 0)
 	{
@@ -16,8 +16,11 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
