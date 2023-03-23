e141d533c6f2af4b0133c337678ce19cdb45b1de
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-23 17:12:21 +0000

    debugging error

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*largest_number - returns the largest of 3 numbers
-*@a: first integer
-*@b: second integer
-*@c: third integer
-*Return: largest number
-*/
+ *largest_number - returns the largest of 3 numbers
+ *@a: first integer
+ *@b: second integer
+ *@c: third interger
+ *Return: largest number
+ */
 
 int largest_number(int a, int b, int c)
 {
@@ -14,7 +14,7 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && a > c)
 	{
-	largest = a;
+		largest = a;
 	}
 	else if (b > a && b > c)
 	{
@@ -22,8 +22,8 @@ int largest_number(int a, int b, int c)
 	}
 	else
 	{
-	largest = c;
+		largest = c;
 	}
-	
+
 	return (largest);
 }
