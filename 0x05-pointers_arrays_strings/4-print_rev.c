a295f2662091edda68ae5bfa7f9a2e3d3e144124
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-28 01:49:21 +0000

    Working with pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_rev - Entry point
+ *@s: input
+ *Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+		;
+	for (i = i - 1; s[i] != '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
