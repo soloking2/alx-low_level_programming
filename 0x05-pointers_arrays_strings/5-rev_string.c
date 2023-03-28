b6feaf7e0f6a8eb4f9a1d92bd2ea51af1273b4ff
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-28 02:15:35 +0000

    Working with pointers

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *rev_string - Entry point
+ *@s: input
+ *Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int i, len;
+	char *start, *end = s;
+
+	for (i = 0; s[i] != '\0' && s[i + 1] != '\0'; i++)
+	{
+		end++;
+	}
+	len = i + 1;
+	start = s;
+	for (i = 0; i < len / 2; i++)
+	{
+		char temp;
+
+		temp = *end;
+		*end = *start;
+		*start = temp;
+		start++;
+		end--;
+	}
+	end[len + 1] = '\0';
+}
