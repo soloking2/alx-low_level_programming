f9d779e11a4e63c59cbf62f0837089b6624bb5b3
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-31 02:19:49 +0000

    Pointers usage

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ **leet - Entry point
+ *@s: input
+ *Return: character
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
