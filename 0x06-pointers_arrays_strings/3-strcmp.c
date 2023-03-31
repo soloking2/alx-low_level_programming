f74f9224707dc73e790129346b59fb2e48161eca
Solomon Umeh <soloking2forsure@gmail.com>
2023-03-31 01:14:28 +0000

    Pointers usage

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
- **_strncpy - Entry point
- *@dest: input
- *@src: input
- *n: input
- *Return: characters
+ **_strcmp - Entry point
+ *@s1: input
+ *@s2: input
+ *Return: number
  */
-char *_strncpy(char *dest, char *src, int n)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	i = 0;
-
-	while (i < n && *(src + i))
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		*(dest + i) = *(src + i);
-		i++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-	while (i < n)
-	{
-		*(dest + i) = '\0';
-		i++;
-	}
-	return (dest);
+	return (0);
 }
