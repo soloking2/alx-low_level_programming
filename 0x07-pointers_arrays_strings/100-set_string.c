6620c0eee81311ff20477c9d5fe639408a2a1627
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-04 00:50:45 +0000

    Pointers, arrays assessment

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - entry point
+ * @s: input
+ * @to: input
+ * 
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
