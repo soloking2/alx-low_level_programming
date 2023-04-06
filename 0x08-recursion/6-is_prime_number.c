a0e6d982bc131905c8a14136fc33369b4abccf87
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-06 01:31:21 +0000

    Recursion exercises

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
+int prime_helper(int num, int divisor);
 /**
  * is_prime_number - entry point
  * @n: input
@@ -8,7 +9,7 @@
  */
 int is_prime_number(int n)
 {
-	int i;
+	int i = 5;
 
 	if (n <= 1)
 		return (0);
@@ -16,10 +17,28 @@ int is_prime_number(int n)
 		return (1);
 	if (n % 2 == 0 || n % 3 == 0)
 		return (0);
-	for (i = 5; i < n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-	return (1);
+	return (prime_helper(n, i));
 }
+
+/**
+ * prime_helper - helper function
+ * @num: the prime value
+ * @divisor: What is used to divider
+ * Return: integer
+ */
+int prime_helper(int num, int divisor)
+{
+	if (divisor < num)
+	{
+		if (num % divisor == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	return (prime_helper(num, divisor + 1));
+}
+
