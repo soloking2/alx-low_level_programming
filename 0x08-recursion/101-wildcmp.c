4af6c40298f4373c815791f456f1bc70a06cce5a
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-06 02:31:50 +0000

    Recursion exercises

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * wildcmp - entry point
+ * @s1: input
+ * @s2: input
+ * Return: integer
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	} else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
