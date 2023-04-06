d586d57986efcd7d72274b8bf5c2139137b50ac1
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-06 00:03:44 +0000

    Recursion exercises

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - entry point
+ * @x: input
+ * @y: input
+ * Return: number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	if(y == 0)
+	{
+		return (1);
+	}
+	return (x *= _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,9 @@
 int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _sqrt_recursion(int n);
 
 #endif
 
