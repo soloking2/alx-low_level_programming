fdadc1fea465cfe2f86abc4d38af2af7ec86ff72
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-06 00:40:54 +0000

    Recursion exercises

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include "main.h"
+
+int sqrt_helper(int root, int num);
 /**
  * _sqrt_recursion - entry point
  * @n: input
  * Return: number
  */
-int sqrt_helper(int root, int num);
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
 		return (-1);
 	}
-	
 	return (sqrt_helper(1, n));
-}
 
+}
 /**
  * sqrt_helper - execute code
- * @root: value to be incremented
- * @num: n value
+ * @root: input
+ * @num: input
  * Return: number
  */
 int sqrt_helper(int root, int num)
