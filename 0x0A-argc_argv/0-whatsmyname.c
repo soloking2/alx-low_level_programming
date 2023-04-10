75f5c75e50385a32e5756ee11561635f154a9bf4
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-10 20:45:13 +0000

    Arguments task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: input
+ * @argv: input
+ * Return: Always Success(0)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
