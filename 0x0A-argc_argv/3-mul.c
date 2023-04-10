cd5cd38066b8ec9275a4ef7e1fef9221488afc6c
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-10 21:15:47 +0000

    Arguments task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: input
+ * @argv: input
+ * Return: Success(0) or Fail (1)
+ */
+int main(int argc, char *argv[])
+{
+	int i, result = 0;
+
+	if (argc > 0 && (argc - 1) == 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			result *= argv[i];
+		}
+		printf("%d\n", result);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
