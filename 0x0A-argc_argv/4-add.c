a25948b4c8f9d0d6059a6d2e8c11fee8d55eda0e
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-10 22:15:28 +0000

    Arguments task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - entry point
+ * @argc: input
+ * @argv: input
+ * Return: Success(0) or Fail (1)
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, result = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (1);
+	} else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			result += atoi(argv[i]);
+		}
+		printf("%d\n", result);
+		return (0);
+	}
+}
