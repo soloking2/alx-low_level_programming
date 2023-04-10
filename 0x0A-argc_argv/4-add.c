6cc5d1202d27922d03f468ed63fd91444deeb417
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-10 22:25:40 +0000

    Arguments task

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,24 +11,18 @@ int main(int argc, char *argv[])
 {
 	int i, j, result = 0;
 
-	if (argc == 1)
+	for (i = 1; i < argc; i++)
 	{
-		printf("%d\n", 0);
-	} else
-	{
-		for (i = 1; i < argc; i++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			for (j = 0; argv[i][j] != '\0'; j++)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
-				if (argv[i][j] < '0' || argv[i][j] > '9')
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-			result += atoi(argv[i]);
 		}
-		printf("%d\n", result);
-		return (0);
+		result += atoi(argv[i]);
 	}
+	printf("%d\n", result);
+	return (0);
 }
