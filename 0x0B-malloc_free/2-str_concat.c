e956e2555f95258cf39a1d772eedc95ee9d9e947
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-12 03:11:03 +0000

    Malloc task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s1 = "";
+		s2 = "";
 	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 	concat = malloc(len * sizeof(char));
