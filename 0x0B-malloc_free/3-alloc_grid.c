39807c3a1cea045d136351088f2f335e3b65b411
Solomon Umeh <soloking2forsure@gmail.com>
2023-04-12 04:36:19 +0000

    Malloc task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,14 +14,19 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	arr = (int **)malloc(height * sizeof(int *));
+	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *)malloc(sizeof(int) * width);
+		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
